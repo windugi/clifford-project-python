904401c1ad8049064505f859824c8f6c4d1c9dbb
windugi <windugigih@gmail.com>
2025-06-11 14:56:45 +0700

    test

---
+++ ask_the_translate.py
@@ -0,0 +1,66 @@
+from tkinter import Tk, messagebox, font, Label, Entry, Button
+import os
+
+print('simple english translate')
+root = Tk()
+root.withdraw()
+
+
+the_world = {}
+def read_from_file():
+    file_path = os.path.join('exam2', 'translate_data.txt')
+    with open(file_path) as file:
+       for line in file:
+           line = line.rstrip('\n')
+           country, city = line.split('/')
+           the_world[country] = city
+read_from_file()
+print(the_world)
+
+def write_to_file(country_name, city_name):
+   with open('m12_capital_data.txt', 'a') as file:
+       file.write('\n' + country_name + '/' + city_name)
+       
+def custom_askstring(title, prompt, dialog_size="450x150", bg_color="lightblue", font_size=12, font_weight="bold"):
+   dialog = Tk()
+   dialog.title(title)
+   dialog.configure(bg=bg_color)
+   custom_font = font.Font(size=font_size, weight=font_weight)
+   label = Label(dialog, text=prompt, bg=bg_color)
+   label.configure(font=custom_font)
+   label.pack()
+   entry = Entry(dialog, font=custom_font)
+   entry.pack()
+   def ok():
+    dialog.result = entry.get()
+    dialog.destroy()
+
+   button = Button(dialog, text="OK", command=ok, bg=bg_color, font=custom_font)
+   button.pack()
+   dialog.geometry(dialog_size)  # Setting Dialog Size
+   dialog.eval('tk::PlaceWindow . center')
+   
+   entry.focus_set()
+   dialog.wait_window()
+
+   try:
+       return dialog.result
+   except AttributeError:
+       return ""    
+
+   
+while True:
+    query_country_input = custom_askstring('Country', 'Type the name of a country:', dialog_size="450x150", bg_color="lightgreen", font_size=14, font_weight="bold")
+    if query_country_input:
+       query_country = query_country_input.capitalize()
+       if query_country in the_world:
+           result = the_world[query_country]
+           messagebox.showinfo('Answer', 'The capital city of ' + query_country + ' is ' + result + '!')          
+       else:
+            new_city = custom_askstring('Teach me', 'I don\'t know! What is the capital city of ' + query_country + '?', dialog_size="450x150", bg_color="lightblue", font_size=12, font_weight="bold")
+            the_world[query_country] = new_city
+            write_to_file(query_country, new_city)
+    else  :
+        break   
+    
+   
---
+++ mexam_countdown_calendar.py
@@ -0,0 +1,51 @@
+from tkinter import Tk, Canvas
+from datetime import datetime
+
+def update_clock():
+    now = datetime.now()
+    current_time = now.strftime('%H:%M:%S')
+    current_date = now.strftime('%d/%m/%Y')
+    c.itemconfig(clock_text, text=f"Date: {current_date}\nTime: {current_time}")
+    root.after(1000, update_clock)  # 1000ms = 1 second
+
+def get_events():
+    list_events = []
+    with open('m11_events.txt') as file:
+        for line in file:
+            line = line.rstrip('\n')
+            current_event = line.split(',')
+            event_date = datetime.strptime(current_event[1], '%d/%m/%y').date()
+            current_event[1] = event_date
+            list_events.append(current_event)
+    return list_events
+
+def days_between_dates(date1, date2):
+    time_between = str(date1 - date2)
+    number_of_days = time_between.split(' ')
+    return number_of_days[0]
+
+root = Tk()
+c = Canvas(root, width=800, height=500, bg='green')
+c.pack()
+c.create_text(100, 50, anchor='w', fill='orange', font='Arial 28 bold underline', text='My Countdown Calendar')
+
+events = get_events()
+today = datetime.now().date()
+vertical_space = 100
+events.sort(key=lambda x: x[1])
+
+for event in events:
+    event_name = event[0]
+    days_until = days_between_dates(event[1], today)
+    display = f"It is {days_until} days until {event_name}"
+    if int(days_until) <= 7:
+        text_col = 'red'
+    else:
+        text_col = 'lightblue'
+    
+    c.create_text(100, vertical_space, anchor='w', fill=text_col, font='Arial 28 bold', text=display)
+    vertical_space += 30  # Increment vertical space after each event
+
+clock_text = c.create_text(750, 400, anchor='e', fill='white', font='Arial 18 bold')
+update_clock()  # Start the live clock update
+root.mainloop()
